839788d70236479b2eaee8b66c662b5630e4a280
chun411630352 <riley20040305@gmail.com>
2024-07-03 10:59:21 +0800

    增加ch2_2、ch2_3

---
+++ ch2_2.java
@@ -0,0 +1,10 @@
+public class ch2_2 {//定義類別ch2_2
+    public static void main(String[]args){
+        int a;//宣告整數a,但不設定初值
+        int b=4,sum;//宣告b和sum，但至將b的值設為4
+
+        a=5;//設定a的值為5
+        sum=a+b;//將sum的值設為a+b
+        System.out.println("sum="+sum);//印出sum的值
+    }
+}
