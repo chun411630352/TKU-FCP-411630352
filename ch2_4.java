a19625ac35819f45fc438e493bd70482033f6696
chun411630352 <riley20040305@gmail.com>
2024-07-03 11:39:30 +0800

    增加ch2_4、ch2_5、ch2_6

---
+++ ch2_4.java
@@ -0,0 +1,9 @@
+public class ch2_4 {
+    public static void main(String[]args){
+        int num1=2; //宣告整數變數num1，並設值為2 
+        int num2=3; //宣告整數變數num2，並設值為3
+
+        System.out.println("I have "+num1+" dogs");//印出運算結果
+        System.out.println("You have "+num2+" dogs");//印出運算結果
+    }
+}
---
+++ ch2_6.java
@@ -0,0 +1,8 @@
+public class ch2_6 {//定義類別ch2_6
+    public static void main(String[]args){ //main()函數，主程式開始
+        int n1=12;
+        int n2=26;
+        
+        System.out.println(n1+"+"+n2+"="+(n1+n2)); //印出n1+n2的值
+    }
+}
