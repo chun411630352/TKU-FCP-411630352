9f135ccea8a3a54274d8c4af88f2370c28e6799f
chun411630352 <riley20040305@gmail.com>
2024-07-09 13:29:57 +0800

    ch4

---
+++ ch4_1.java
@@ -0,0 +1,12 @@
+public class ch4_1 {
+    public static void main(String[] args) {
+        
+        int a=9,b=5;
+        
+        System.out.printf("%d + %d=%d\n",a,b,a+b);  //相加
+        System.out.printf("%d - %d=%d\n",a,b,a-b);  //相減
+        System.out.printf("%d * %d=%d\n",a,b,a*b);  //相乘
+        System.out.printf("%d / %d=%d\n",a,b,a/b);  //相除
+        System.out.printf("%d %% %d=%d\n",a,b,a%b); //取餘數
+    }
+}
